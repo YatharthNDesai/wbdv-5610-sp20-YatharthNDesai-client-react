1cab55c179861d29b03b77dd40743ca98d42503b
YatharthDesai <36489137+YatharthDesai@users.noreply.github.com>
2020-02-05 17:35:48 -0500

    Course card update

--- src/components/CourseCardComponent.js
+++ src/components/CourseCardComponent.js
@@ -3,10 +3,14 @@ import "../stylesheets/CourseGridComponent.css"
 
 class CourseCardComponent extends React.Component {
 
+    state = {
+        editing: false
+    }
+
     render() {
         return (
 
-            <div className="wbdv-card col-xs-11 col-sm-5 col-md-8 col-lg-8 col-xl-2 m-1" style={{position:"relative"}}>
+            <div className="wbdv-card col-xs-10 col-sm-6 col-md-4 col-lg-3 col-xl-2" style={{position:"relative"}}>
                 <div className="col-3" style={{position: "absolute", right: 0}}>
                     <button onClick={() => this.props.deleteCourse(this.props.course)} className="btn"
                             type="button">
@@ -20,17 +24,32 @@ class CourseCardComponent extends React.Component {
 
 <div className="row">
     <div className="col-1">
-        <i className="fas fa-file wbdv-row wbdv-icon m-2"></i></div>
-    <div className="col-6">
-                <a onClick={this.props.showEditor} href="#">
+        <i className="fas fa-file wbdv-row wbdv-icon m-1"></i></div>
+
+    <div className="col-4">
+        {!this.state.editing &&  <a onClick={this.props.showEditor} href="#">
                     {this.props.course.title}
-                </a></div>
+                </a>}
+        {this.state.editing && <input style={{width:"100px"}}/>}</div>
 
                 <div className="col-2" style={{ float:"right"}} >
-                    <button className="btn" type="btn">
+                    {!this.state.editing &&
+                    <button className="btn" type="btn"
+                            onClick={() => {
+                                this.setState({
+                                                  editing: true
+                                              })
+                            }}>
                         <i className="fas fa-edit"></i>
-                    </button>
+                    </button>}
                 </div>
+    <div className="col-2">
+        {this.state.editing && <button className="btn" onClick={() => {
+            this.setState({
+                              editing: false
+                          })
+        }}><i className="fa fa-save"/> </button>}
+    </div>
 </div>
                 <div style={{position: "absolute", bottom: 2}}>
                     <div className="col-3">Modified:{this.props.course.lastModified}</div>
